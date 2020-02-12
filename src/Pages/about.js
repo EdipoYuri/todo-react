036fd70521dc8a1343e4f91548bb7780d4636edb
Édipo Yuri Ribeiro da Silva <edipoyurirs@gmail.com>
2020-02-12 15:52:24 -0300

    Teste usando API externa
    Apenas um pequeno teste usando APIs externas.
    O arquivo 'about.js' usa uma API de Yu-Gi-Oh! na qual retorna informações e a imagem da carta buscada.
    O objetivo é apenas entender como que faz para pegar e mostrar os dados, então não é uma página bonita e muito menos usável; será removida logo mais.

--- src/Pages/about.js
+++ src/Pages/about.js
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../App.css'
 
 const About = () => {
-    async function teste(){
-        const response = await fetch('https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes')
-        const jsonData = await response.json();
-        console.log(jsonData);
-        
+    const [data, setData] = useState([]);
+    const [cardInfo, setCardInfo] = useState(null);
+
+    const handleCardSearch = (event) => {
+        setCardInfo(event.target.value)
+    }
+
+    const onSubmitSearchCard = (event) => {
+        event.preventDefault()
+        teste()
+    }
+
+    const teste = async () => {
+        const response = await fetch('https://db.ygoprodeck.com/api/v6/cardinfo.php')
+        const jsonData = await response.json()
+        const card = jsonData.filter((item, index) => item.name === cardInfo)
+        console.log("Entrou")
+        console.log(card)
+        setData(card)
     }
 
     return(
-        <div>   
-            <h1>Olá... É isso ai</h1>
-            <button onClick={() => teste()}>TESTE</button>
+        <div>
+            <form onSubmit={onSubmitSearchCard}>   
+                <input type="text" onChange={handleCardSearch} />
+                <button > BUSCAR </button>
+            </form>
+            <ul className="task-list">
+                {data.map((item, index) => 
+                    <li key={index}>
+                        <p>Name : {item.name} | Type : {item.type}</p>
+                        <p>Atk  : {item.atk} | Def  : {item.def} | Level: {item.level}</p>
+                        <p>Image: </p>
+                        <img src={item.card_images[0].image_url} alt="Card image" />
+                    </li>
+                )}
+            </ul>
         </div>
     )
 }
